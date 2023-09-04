0dbe8b3fd2b41a81bfa656e15905e41c0c7e29df
jasminisantos <jasminirebeca10@gmail.com>
2023-09-04 14:07:56 -0300

    radar model

--- Mini2/Model/Radar.swift
+++ Mini2/Model/Radar.swift
@@ -5,4 +5,48 @@
 //  Created by Jasmini Rebecca Gomes dos Santos on 31/08/23.
 //
 
-import Foundation
+class Radar {
+    private let map: Map
+    
+    init(map: Map) {
+        self.map = map
+    }
+    
+    private func contaminationLevelAt(x: Int, y: Int) -> Int? {
+        if x >= 0 && x < map.columns && y >= 0 && y < map.rows {
+            return map.rooms[y][x].getContaminationLevel()
+        }
+        return nil
+    }
+    
+    func topLeftQuadrantContamination() -> [Int?] {
+        let left = contaminationLevelAt(x: map.currentX + 1, y: map.currentY)
+        let diagonal = contaminationLevelAt(x: map.currentX + 1, y: map.currentY + 1)
+        let top = contaminationLevelAt(x: map.currentX, y: map.currentY + 1)
+        return [left, diagonal, top]
+    }
+
+    func topRightQuadrantContamination() -> [Int?] {
+        let right = contaminationLevelAt(x: map.currentX - 1, y: map.currentY)
+        let diagonal = contaminationLevelAt(x: map.currentX - 1, y: map.currentY + 1)
+        let top = contaminationLevelAt(x: map.currentX, y: map.currentY + 1)
+        return [right, diagonal, top]
+    }
+
+    func bottomLeftQuadrantContamination() -> [Int?] {
+        let left = contaminationLevelAt(x: map.currentX + 1, y: map.currentY)
+        let diagonal = contaminationLevelAt(x: map.currentX + 1, y: map.currentY - 1)
+        let down = contaminationLevelAt(x: map.currentX, y: map.currentY - 1)
+        return [left, diagonal, down]
+    }
+
+    func bottomRightQuadrantContamination() -> [Int?] {
+        let right = contaminationLevelAt(x: map.currentX - 1, y: map.currentY)
+        let diagonal = contaminationLevelAt(x: map.currentX - 1, y: map.currentY - 1)
+        let down = contaminationLevelAt(x: map.currentX, y: map.currentY - 1)
+        return [right, diagonal, down]
+    }
+}
+
+
+
